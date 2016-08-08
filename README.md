78a23ed64e59405cc9c019466d57e954b4180ab8
Niraj Pant <nirajpant7@gmail.com>
2016-08-08 09:58:49 -0700

    Allow for per-thread counting

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ messenger-wordcloud was inspired by [fb-chat-wordcloud](https://github.com/jpnel
  - Navigate to [Download Your Information](https://www.facebook.com/dyi) on Facebook. Click on "Download a copy of your Facebook data."
 	 <img src="screenshots/dyi.png" style="width: 50%; height: 50%"/>
  - After FB emails you a link to a zip file, unzip the archive, and copy `html/messages.htm` into the root directory of this cloned repo.
- - Then, run `python app.py` and you're all set! A wordcloud and stats will be generated from your messages. This process may take a few minutes.
+ - Then, run `python app.py` and voilà! A wordcloud and stats will be generated from your messages (the entire process may take a few minutes).
+
+## Example Output
+
 
 ## License
 messenger-wordcloud is licensed under the MIT License. See [`LICENSE`](LICENSE) for the full
--- app.py
+++ app.py
@@ -2,61 +2,73 @@ from bs4 import BeautifulSoup
 import ipdb
 from os import path
 from wordcloud import WordCloud
-from colorama import init
+from colorama import init, Fore, Back, Style
 init()
 import nltk
 nltk.download("stopwords")
 from nltk.corpus import stopwords
+#import matplotlib.pyplot as plt
+import re
 
-f = open('messages.htm', 'r')
-soup = BeautifulSoup(f, 'html.parser')
+raw_messages = open('messages.htm', 'r')
+soup = BeautifulSoup(raw_messages, 'html.parser')
 
-thing = soup.find_all("div", class_="thread")
+#ipdb.set_trace()
 
-#print(thing[4])
-print("# of threads:")
-print(len(thing))
+threads = soup.find_all("div", class_="thread")
+#print(Fore.BLUE + "# of threads: \n" + str(len(threads)))
 
-
-thing2 = soup.findAll('p')
-print("# of messages:")
-print(len(thing2))
+messages = soup.findAll('p')
+#print(Fore.BLUE + "# of messages: \n" + str(len(messages)));
 
 random_var = []
 
-for ps in thing2:
-    val = ' '.join(ps.stripped_strings)
+for message in messages:
+    val = ' '.join(message.stripped_strings)
     random_var.append(val)
 
 filtered_words = [word for word in random_var if word not in stopwords.words('english')]
 
 final_str = " ".join(filtered_words).encode('utf-8').strip()
 
+for thread in soup.find_all("div", class_="thread"):
+    soup = BeautifulSoup(str(thread), 'html.parser')
+    message_count = len(soup.find_all("p"))
+    p = re.compile('.*"thread">(.*)')
+    print("Thread: ")
+    print(p.match(str(thread)).group(1))
+    print("# of messages:")
+    print(message_count)
+    print("% of all messages:")
+    print(str(float(message_count)/len(messages)))
+
+
 ftwo = open('out.txt', 'w')
 ftwo.write(final_str)
 ftwo.close()
-print('successfully outputted')
-
-#print colored('hello', 'red'), colored('world', 'green')
+#print('successfully outputted')
 
 d = path.dirname(__file__)
 #
 # Read the whole text.
 text = open(path.join(d, 'out.txt')).read()
 
+
+
+
+
 # Generate a word cloud image
-wordcloud = WordCloud(width=800, height=400).generate(text)
+#wordcloud = WordCloud(width=1600, height=800).generate(text)
 
 # Display the generated image:
 # the matplotlib way:
-import matplotlib.pyplot as plt
-plt.imshow(wordcloud)
-plt.axis("off")
+#plt.imshow(wordcloud)
+#plt.axis("off")
 
 # take relative word frequencies into account, lower max_font_size
-wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(text)
-plt.figure( figsize=(20,10), facecolor='k' )
-plt.imshow(wordcloud)
-plt.tight_layout(pad=0)
-plt.axis("off")
-plt.show()
+#wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(text)
+#plt.figure( figsize=(20,10), facecolor='k' )
+#plt.imshow(wordcloud)
+#plt.tight_layout(pad=0)
+#plt.axis("off")
+#plt.show()
